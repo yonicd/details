5904a8a9c6caf47366f6394fe9641947ef334bc4
Jonathan Sidi <yonis@metrumrg.com>
2019-09-09 18:37:58 -0400

    update device tests

--- tests/testthat/test-device.R
+++ tests/testthat/test-device.R
@@ -2,14 +2,30 @@ testthat::context('device')
 
 testthat::describe('using device',{
   
-  skip_on_travis()
-  skip_if_offline()
-  
-  it('plots',{
-    testthat::expect_output(
-      print(details(plot(x=mtcars$mpg,y=mtcars$wt))),
-      regexp = 'i.imgur.com'
+  tf <- file.path(getwd(),'test.png')
+  file.create(tf)
+  testenv <- new.env()
+    
+  ret <- device_check(
+    plot(x=mtcars$mpg,y=mtcars$wt),
+    f_png = tf,
+    env = testenv
     )
+
+  it('device flag',{
+    testthat::expect_true(testenv$device)
   })
+  
+  it('device png',{
+    testthat::expect_true(file.size(testenv$f_png)>0)
+  })
+  
+  it('device upload',{
+    skip_if_offline()
+    skip_on_travis()
+    testthat::expect_true(grepl('imgur',imgur_upload(testenv$f_png)))
+  })
+  
+  unlink(tf)
   
 })
