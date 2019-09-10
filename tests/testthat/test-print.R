8d3292b4097d920279b27c4965294682c92107fb
Jonathan Sidi <yonis@metrumrg.com>
2019-09-09 22:38:00 -0400

    add skips on appveyor [skip travis]

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,22 +1,5 @@
 testthat::context('print')
 
-testthat::describe('print methods',{
-  it('console',{
-    testthat::expect_output(
-      print(details::details('test')),
-      regexp = bench_fun('test')
-    )    
-  })
-  
-  it('character',{
-    testthat::expect_equal(
-      unlist(strsplit(bench_fun('test'),'\\n')),
-      print(details::details('test',output = 'character'))
-    )    
-  })
-  
-})
-
 testthat::describe('print clipr',{
   testthat::skip_on_travis()
   skip_if_no_clipboard()
@@ -30,4 +13,23 @@ testthat::describe('print clipr',{
       clipr::read_clip()
     )
   })
+})
+
+testthat::describe('print methods',{
+  it('console',{
+    testthat::skip_on_appveyor()
+    testthat::expect_output(
+      print(details::details('test')),
+      regexp = bench_fun('test')
+    )    
+  })
+  
+  it('character',{
+    testthat::skip_on_appveyor()
+    testthat::expect_equal(
+      unlist(strsplit(bench_fun('test'),'\\n')),
+      print(details::details('test',output = 'character'))
+    )    
+  })
+  
 })
