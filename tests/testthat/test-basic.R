df29b933776a5fd980b73444b9fd1b3173cd18c7
Jonathan Sidi <yonis@metrumrg.com>
2019-10-31 10:06:50 -0400

    patch for non file singleton objects

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -15,14 +15,21 @@ testthat::describe('basic',{
       unlist(strsplit(bench_fun(mtcars),'\\n'))
     )
   })
-  
+
   it('file',{
     testthat::expect_equal(
       print(details::details(object = 'helpers.R',output = 'character')),
       unlist(strsplit(bench_fun(readLines('helpers.R')),'\\n'))
     )
   })
-  
+
+  it('non file singleton',{
+    testthat::expect_equal(
+      print(details::details(mtcars[1,1],output = 'character')),
+      unlist(strsplit(bench_fun(mtcars[1,1]),'\\n'))
+    )
+  })
+    
   it('no lang',{
     testthat::expect_equal(
       print(details::details(object = 'helpers.R',output = 'character',lang = NULL)),
