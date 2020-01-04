499106eb2a3fb0200f1c35ffe7c12cce8d5626ce
Jonathan Sidi <yonis@metrumrg.com>
2020-01-03 23:16:00 -0500

    add knitr details engine

---
+++ R/engine.R
@@ -0,0 +1,43 @@
+`%n%` <- function (x, y){
+  if (is.null(x)) y else x  
+}
+
+eng_detail <- function (options) {
+  type <- options$type %n% "details"
+  options$type <- 'r'
+  
+  options$details.lang <- options$details.lang %n% 'r'
+  options$details.summary <- options$details.summary %n% NULL
+  options$details.tooltip <- options$details.tooltip %n% NULL
+  options$details.open <- options$details.open %n% FALSE
+  
+  code <- details::details(eval(parse(text = options$code)),
+                           lang = options$details.lang,
+                           tooltip = options$details.tooltip,
+                           summary = options$details.summary,
+                           open = options$details.open,
+                           output = 'character')
+  if(options$echo){
+    echo_code <- sprintf('```r\n%s\n```',paste0(options$code,collapse = '\n'))
+    options$code <- paste0(c(echo_code,code),collapse = '\n')
+  }else{
+    options$code <- paste0(code,collapse = '\n')  
+    options$echo <- TRUE
+  }
+  
+  get('eng_asis',envir = asNamespace('knitr'))(options)
+}
+
+
+register_eng_detail = function(envs, engine) {
+  knitr::knit_engines$set(setNames(lapply(envs, function(env) {
+    function(options) {
+      options$type = env
+      engine(options)
+    }
+  }), envs))
+}
+
+.onLoad = function(lib, pkg) {
+  register_eng_detail('details', eng_detail)
+}
--- data-raw/test.md
+++ data-raw/test.md
@@ -1,7 +1,79 @@
+details chunk engine
+================
+
+``` r
+library(details)
+```
+
+## details chunks
+
+### echo = TRUE & details closed
+
+``` r
+x <- 1
+x * 2
+```
+
+<details closed>
+
+<summary> click to see eval </summary>
+
+``` r
+
+[1] 2
+```
+
+</details>
+
+<br>
+
+### echo = TRUE & details open
+
+``` r
+x <- 1
+x * 2
+```
+
+<details open>
+
+<summary> click to hide eval </summary>
+
+``` r
+
+[1] 2
+```
+
+</details>
+
+<br>
+
+### echo = FALSE & details open
+
+<details closed>
+
+<summary> open to see eval </summary>
+
+``` r
+
+[1] 2
+```
+
+</details>
+
+<br>
+
+### file contents with no highlighting
+
+<details closed>
+
+<summary> open to see contents </summary>
+
+``` 
+
 Package: details
 Title: Create Details HTML Tag for Markdown and Package
     Documentation
-Version: 0.1.4
+Version: 0.1.3
 Authors@R: 
     person(given = "Jonathan",
            family = "Sidi",
@@ -36,3 +108,17 @@ Encoding: UTF-8
 LazyData: false
 Roxygen: list(markdown = TRUE)
 RoxygenNote: 6.1.1
+```
+
+</details>
+
+<br>
+
+## r chunk
+
+``` r
+x <- 1
+x * 2
+```
+
+    ## [1] 2
